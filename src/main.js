572251f6beadf5394913fe1114ebfd4da108649a
jSuttipong <37823981+jSuttipong@users.noreply.github.com>
2019-02-01 23:40:20 +0700

    no message

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 /* eslint-disable */
+/* eslint-disable no-new */
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -74,7 +75,8 @@ Vue.use(VueGoogleMaps, {
 // })
 
 // app.listen(8080, () => console.log('Hello world'))
-/* eslint-disable no-new */
+Vue.config.productionTip = false
+
 new Vue({
   el: '#app',
   router,
